2b6f3bd45a6097a96275365476e5a01d9d2faaba
Blue <geweidong525@sina.com>
2017-04-11 10:02:06 +0800

    combine the ssid and spotInspection config file

--- sh/scan_ssid.sh
+++ sh/scan_ssid.sh
@@ -3,7 +3,7 @@ n=0
 for i in `iw dev wlan0 scan | grep SSID| awk -F'[:]' '{print $2}'`
 do
 	echo here is :----$i----
-	uci set spotInspection.ssid."ssid$n"="$i"
+	uci set spotInspection.ssidList."ssid$n"="$i"
 	n=$(($n+1))
 done
 uci commit
